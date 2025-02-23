e68193351e85597eaafa731473ba489d3a690831
JUNIKFILM <junikflim@gmail.com>
2025-02-23 13:37:09 -0700

    Update scrip(github).js

--- scrip(github).js
+++ scrip(github).js
@@ -183,10 +183,5 @@ function ocultar_boton(){
  
 }
 
-document.getElementById("sharecomp").addEventListener("click", m_boton);
-
-function m_boton(){
-  boton.style.display = "block";
- 
-}    
+  
 
